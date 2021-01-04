41ffffe97d237bd96796856afb23d0faff52c42d
Nicholas Scifert <70296322+nscifert@users.noreply.github.com>
2021-01-04 02:02:14 -0600

    Add files via upload

---
+++ Main Form.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.questionNumLabel = New System.Windows.Forms.Label()
+        Me.aRadioButton = New System.Windows.Forms.RadioButton()
+        Me.bRadioButton = New System.Windows.Forms.RadioButton()
+        Me.cRadioButton = New System.Windows.Forms.RadioButton()
+        Me.dRadioButton = New System.Windows.Forms.RadioButton()
+        Me.questionTextBox = New System.Windows.Forms.TextBox()
+        Me.TblGameBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TriviaDataSet = New Trivia_Project.TriviaDataSet()
+        Me.aTextBox = New System.Windows.Forms.TextBox()
+        Me.bTextBox = New System.Windows.Forms.TextBox()
+        Me.cTextBox = New System.Windows.Forms.TextBox()
+        Me.dTextBox = New System.Windows.Forms.TextBox()
+        Me.submitButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.TblGameTableAdapter = New Trivia_Project.TriviaDataSetTableAdapters.tblGameTableAdapter()
+        Me.TableAdapterManager = New Trivia_Project.TriviaDataSetTableAdapters.TableAdapterManager()
+        Me.newButton = New System.Windows.Forms.Button()
+        Me.changeButton = New System.Windows.Forms.Button()
+        Me.previousButton = New System.Windows.Forms.Button()
+        Me.nextButton = New System.Windows.Forms.Button()
+        Me.incorrectButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.TblGameBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TriviaDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'questionNumLabel
+        '
+        Me.questionNumLabel.Location = New System.Drawing.Point(13, 13)
+        Me.questionNumLabel.Name = "questionNumLabel"
+        Me.questionNumLabel.Size = New System.Drawing.Size(74, 17)
+        Me.questionNumLabel.TabIndex = 11
+        '
+        'aRadioButton
+        '
+        Me.aRadioButton.AutoSize = True
+        Me.aRadioButton.Checked = True
+        Me.aRadioButton.Location = New System.Drawing.Point(17, 114)
+        Me.aRadioButton.Name = "aRadioButton"
+        Me.aRadioButton.Size = New System.Drawing.Size(34, 21)
+        Me.aRadioButton.TabIndex = 3
+        Me.aRadioButton.TabStop = True
+        Me.aRadioButton.Text = "&A"
+        Me.aRadioButton.UseVisualStyleBackColor = True
+        '
+        'bRadioButton
+        '
+        Me.bRadioButton.AutoSize = True
+        Me.bRadioButton.Location = New System.Drawing.Point(17, 160)
+        Me.bRadioButton.Name = "bRadioButton"
+        Me.bRadioButton.Size = New System.Drawing.Size(33, 21)
+        Me.bRadioButton.TabIndex = 4
+        Me.bRadioButton.Text = "&B"
+        Me.bRadioButton.UseVisualStyleBackColor = True
+        '
+        'cRadioButton
+        '
+        Me.cRadioButton.AutoSize = True
+        Me.cRadioButton.Location = New System.Drawing.Point(17, 206)
+        Me.cRadioButton.Name = "cRadioButton"
+        Me.cRadioButton.Size = New System.Drawing.Size(34, 21)
+        Me.cRadioButton.TabIndex = 5
+        Me.cRadioButton.Text = "&C"
+        Me.cRadioButton.UseVisualStyleBackColor = True
+        '
+        'dRadioButton
+        '
+        Me.dRadioButton.AutoSize = True
+        Me.dRadioButton.Location = New System.Drawing.Point(17, 252)
+        Me.dRadioButton.Name = "dRadioButton"
+        Me.dRadioButton.Size = New System.Drawing.Size(35, 21)
+        Me.dRadioButton.TabIndex = 6
+        Me.dRadioButton.Text = "&D"
+        Me.dRadioButton.UseVisualStyleBackColor = True
+        '
+        'questionTextBox
+        '
+        Me.questionTextBox.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.questionTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblGameBindingSource, "Question", True))
+        Me.questionTextBox.Location = New System.Drawing.Point(16, 33)
+        Me.questionTextBox.Multiline = True
+        Me.questionTextBox.Name = "questionTextBox"
+        Me.questionTextBox.ReadOnly = True
+        Me.questionTextBox.Size = New System.Drawing.Size(461, 51)
+        Me.questionTextBox.TabIndex = 12
+        '
+        'TblGameBindingSource
+        '
+        Me.TblGameBindingSource.DataMember = "tblGame"
+        Me.TblGameBindingSource.DataSource = Me.TriviaDataSet
+        '
+        'TriviaDataSet
+        '
+        Me.TriviaDataSet.DataSetName = "TriviaDataSet"
+        Me.TriviaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'aTextBox
+        '
+        Me.aTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblGameBindingSource, "AnswerA", True))
+        Me.aTextBox.Location = New System.Drawing.Point(56, 113)
+        Me.aTextBox.Multiline = True
+        Me.aTextBox.Name = "aTextBox"
+        Me.aTextBox.ReadOnly = True
+        Me.aTextBox.Size = New System.Drawing.Size(421, 31)
+        Me.aTextBox.TabIndex = 7
+        '
+        'bTextBox
+        '
+        Me.bTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblGameBindingSource, "AnswerB", True))
+        Me.bTextBox.Location = New System.Drawing.Point(56, 159)
+        Me.bTextBox.Multiline = True
+        Me.bTextBox.Name = "bTextBox"
+        Me.bTextBox.ReadOnly = True
+        Me.bTextBox.Size = New System.Drawing.Size(421, 31)
+        Me.bTextBox.TabIndex = 8
+        '
+        'cTextBox
+        '
+        Me.cTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblGameBindingSource, "AnswerC", True))
+        Me.cTextBox.Location = New System.Drawing.Point(56, 205)
+        Me.cTextBox.Multiline = True
+        Me.cTextBox.Name = "cTextBox"
+        Me.cTextBox.ReadOnly = True
+        Me.cTextBox.Size = New System.Drawing.Size(421, 31)
+        Me.cTextBox.TabIndex = 9
+        '
+        'dTextBox
+        '
+        Me.dTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TblGameBindingSource, "AnswerD", True))
+        Me.dTextBox.Location = New System.Drawing.Point(57, 251)
+        Me.dTextBox.Multiline = True
+        Me.dTextBox.Name = "dTextBox"
+        Me.dTextBox.ReadOnly = True
+        Me.dTextBox.Size = New System.Drawing.Size(421, 31)
+        Me.dTextBox.TabIndex = 10
+        '
+        'submitButton
+        '
+        Me.submitButton.Location = New System.Drawing.Point(497, 113)
+        Me.submitButton.Name = "submitButton"
+        Me.submitButton.Size = New System.Drawing.Size(113, 32)
+        Me.submitButton.TabIndex = 0
+        Me.submitButton.Text = "&Submit Answer"
+        Me.submitButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(497, 281)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(113, 32)
+        Me.exitButton.TabIndex = 2
+        Me.exitButton.Text = "E&xit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'TblGameTableAdapter
+        '
+        Me.TblGameTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.tblGameTableAdapter = Me.TblGameTableAdapter
+        Me.TableAdapterManager.UpdateOrder = Trivia_Project.TriviaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'newButton
+        '
+        Me.newButton.Enabled = False
+        Me.newButton.Location = New System.Drawing.Point(497, 239)
+        Me.newButton.Name = "newButton"
+        Me.newButton.Size = New System.Drawing.Size(113, 32)
+        Me.newButton.TabIndex = 1
+        Me.newButton.Text = "&New Game"
+        Me.newButton.UseVisualStyleBackColor = True
+        '
+        'changeButton
+        '
+        Me.changeButton.Location = New System.Drawing.Point(497, 155)
+        Me.changeButton.Name = "changeButton"
+        Me.changeButton.Size = New System.Drawing.Size(113, 32)
+        Me.changeButton.TabIndex = 13
+        Me.changeButton.Text = "C&hange Answer"
+        Me.changeButton.UseVisualStyleBackColor = True
+        '
+        'previousButton
+        '
+        Me.previousButton.Enabled = False
+        Me.previousButton.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.previousButton.Location = New System.Drawing.Point(497, 29)
+        Me.previousButton.Name = "previousButton"
+        Me.previousButton.Size = New System.Drawing.Size(113, 32)
+        Me.previousButton.TabIndex = 14
+        Me.previousButton.Text = "&Previous Question"
+        Me.previousButton.UseVisualStyleBackColor = True
+        '
+        'nextButton
+        '
+        Me.nextButton.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nextButton.Location = New System.Drawing.Point(497, 71)
+        Me.nextButton.Name = "nextButton"
+        Me.nextButton.Size = New System.Drawing.Size(113, 32)
+        Me.nextButton.TabIndex = 15
+        Me.nextButton.Text = "&Next Question"
+        Me.nextButton.UseVisualStyleBackColor = True
+        '
+        'incorrectButton
+        '
+        Me.incorrectButton.Enabled = False
+        Me.incorrectButton.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.incorrectButton.Location = New System.Drawing.Point(497, 197)
+        Me.incorrectButton.Name = "incorrectButton"
+        Me.incorrectButton.Size = New System.Drawing.Size(113, 32)
+        Me.incorrectButton.TabIndex = 16
+        Me.incorrectButton.Text = "Number &Incorrect"
+        Me.incorrectButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(399, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 17)
+        Me.Label1.TabIndex = 17
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(622, 323)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.incorrectButton)
+        Me.Controls.Add(Me.nextButton)
+        Me.Controls.Add(Me.previousButton)
+        Me.Controls.Add(Me.changeButton)
+        Me.Controls.Add(Me.newButton)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.submitButton)
+        Me.Controls.Add(Me.dTextBox)
+        Me.Controls.Add(Me.cTextBox)
+        Me.Controls.Add(Me.bTextBox)
+        Me.Controls.Add(Me.aTextBox)
+        Me.Controls.Add(Me.questionTextBox)
+        Me.Controls.Add(Me.dRadioButton)
+        Me.Controls.Add(Me.cRadioButton)
+        Me.Controls.Add(Me.bRadioButton)
+        Me.Controls.Add(Me.aRadioButton)
+        Me.Controls.Add(Me.questionNumLabel)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Trivia Game"
+        CType(Me.TblGameBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TriviaDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents questionNumLabel As Label
+    Friend WithEvents aRadioButton As RadioButton
+    Friend WithEvents bRadioButton As RadioButton
+    Friend WithEvents cRadioButton As RadioButton
+    Friend WithEvents dRadioButton As RadioButton
+    Friend WithEvents questionTextBox As TextBox
+    Friend WithEvents aTextBox As TextBox
+    Friend WithEvents bTextBox As TextBox
+    Friend WithEvents cTextBox As TextBox
+    Friend WithEvents dTextBox As TextBox
+    Friend WithEvents submitButton As Button
+    Friend WithEvents exitButton As Button
+    Friend WithEvents TriviaDataSet As TriviaDataSet
+    Friend WithEvents TblGameBindingSource As BindingSource
+    Friend WithEvents TblGameTableAdapter As TriviaDataSetTableAdapters.tblGameTableAdapter
+    Friend WithEvents TableAdapterManager As TriviaDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents newButton As Button
+    Friend WithEvents changeButton As Button
+    Friend WithEvents previousButton As Button
+    Friend WithEvents nextButton As Button
+    Friend WithEvents incorrectButton As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Main Form.vb
@@ -0,0 +1,219 @@
+﻿Option Explicit On
+Option Strict On
+Option Infer Off
+' Project name:         Trivia Project
+' Project purpose:      Displays trivia questions and
+'                       answers and the number of incorrect
+'                       answers made by the user
+' Created/revised by:   Nicholas Scifert on 09/26/2020
+
+Imports System.ComponentModel.Design
+Imports System.Text
+
+Public Class MainForm
+
+
+    Private results(8) As String                                        'Holds a string that lets the program know the the problem is right or wrong
+
+    'Function used to activate and deactivate the text boxes and buttons 
+    'to allow the user to know that there answer has been submited
+    Private Sub IsActive(ByVal var As String)
+
+        Dim x As String
+
+        x = var
+
+        If results(TblGameBindingSource.Position) = String.Empty Then
+            'if answer has not been submited (Activate text boxes and buttons)
+            questionTextBox.Enabled = True
+            aTextBox.Enabled = True
+            bTextBox.Enabled = True
+            cTextBox.Enabled = True
+            dTextBox.Enabled = True
+            aRadioButton.Enabled = True
+            bRadioButton.Enabled = True
+            cRadioButton.Enabled = True
+            dRadioButton.Enabled = True
+
+            changeButton.Enabled = False                'Stops the user from using the change button
+            submitButton.Enabled = True                 'Allows the user to use the submit button
+
+        Else
+            'if answer has been submited  (Deactivate text boxes and buttons)
+            questionTextBox.Enabled = False
+            aTextBox.Enabled = False
+            bTextBox.Enabled = False
+            cTextBox.Enabled = False
+            dTextBox.Enabled = False
+            aRadioButton.Enabled = False
+            bRadioButton.Enabled = False
+            cRadioButton.Enabled = False
+            dRadioButton.Enabled = False
+
+            changeButton.Enabled = True                 'Allows the user to use the change button
+            submitButton.Enabled = False                'Stops the user from using the submit button
+            incorrectButton.Enabled = True              'Allows the user to use the number incorrect button
+
+        End If
+
+    End Sub
+
+    'exits the program
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'TriviaDataSet.tblGame' table. You can move, or remove it, as needed.
+        Me.TblGameTableAdapter.Fill(Me.TriviaDataSet.tblGame)
+
+        'Displays the quesiton numbers
+        questionNumLabel.Text = "Question " + (TblGameBindingSource.Position + 1).ToString + ":"
+
+
+    End Sub
+
+    Private Sub newButton_Click(sender As Object, e As EventArgs) Handles newButton.Click
+        ' starts a new game
+        TblGameBindingSource.MoveFirst()
+        questionNumLabel.Text = "Question " + (TblGameBindingSource.Position + 1).ToString + ":"
+
+        'resets the results Array
+        For i As Integer = 0 To 8
+            results(i) = String.Empty
+        Next i
+
+
+
+        Dim answerPosition As Integer                   'puts a value of 0 in questionAnswered Array to indicated the question
+
+
+        nextButton.Enabled = True                       'lets the next button be used by the user
+        previousButton.Enabled = False                  'the previous button will not beable to be used by the user
+
+        Call IsActive(results(answerPosition))     'Function Call to the IsActive Function 
+
+    End Sub
+
+    Private Sub submitButton_Click(sender As Object, e As EventArgs) Handles submitButton.Click
+        ' determines whether the user's answer is correct
+        ' and the number of incorrect answers
+
+        Dim answerPosition As Integer
+
+        answerPosition = TblGameBindingSource.Position          'Stores the position of the quesiton 
+
+        Dim userAnswer As String                                'Stores which answer the user submited
+        Dim ptrPosition As Integer
+
+        ' store record pointer's position
+        ptrPosition = TblGameBindingSource.Position             'Assigns the current record's position to the ptrPosition variable
+
+
+        ' determine selected radio button
+        Select Case True
+            Case aRadioButton.Checked
+                userAnswer = aRadioButton.Text.Substring(1, 1)
+            Case bRadioButton.Checked
+                userAnswer = bRadioButton.Text.Substring(1, 1)
+            Case cRadioButton.Checked
+                userAnswer = cRadioButton.Text.Substring(1, 1)
+            Case Else
+                userAnswer = dRadioButton.Text.Substring(1, 1)
+        End Select
+
+        ' updates the number of incorrect answers
+        If userAnswer <>
+                TriviaDataSet.tblGame(ptrPosition).CorrectAnswer Then
+            results(ptrPosition) = "W"                     'Answer has been answwered wrong
+        Else
+            results(ptrPosition) = "C"                     'Answer has been answered correct
+        End If
+
+
+
+        Call IsActive(results(answerPosition))         'Function Call to the IsActive Function
+
+        newButton.Enabled = True                            'Allows the user to use the next button
+
+
+    End Sub
+
+
+    Private Sub nextButton_Click(sender As Object, e As EventArgs) Handles nextButton.Click
+        TblGameBindingSource.MoveNext()                     'moves the record pointer to the next record in the dataset
+
+        questionNumLabel.Text = "Question " + (TblGameBindingSource.Position + 1).ToString + ":"
+
+        Dim answerPosition As Integer
+
+        answerPosition = TblGameBindingSource.Position      'Stores the quesitons postion in the dataset
+
+        Call IsActive(results(answerPosition))         'Calls the IsActive Functiion 
+
+        incorrectButton.Enabled = False                     'Stops the user from using the number incorrect button
+        previousButton.Enabled = True                       'Allows the user to use the previous button
+
+        If TblGameBindingSource.Position + 1 >= 9 Then      'Checks to see if the record pointer is equal to the amount of quesitons in the dataset
+            nextButton.Enabled = False                      'If they are equal, the user can not use the next button at this time
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub previousButton_Click(sender As Object, e As EventArgs) Handles previousButton.Click
+        TblGameBindingSource.MovePrevious()                 'Moves the record pointer to the previous record in the dataset
+
+        questionNumLabel.Text = "Question " + (TblGameBindingSource.Position + 1).ToString + ":"
+
+        Dim answerPosition As Integer
+
+        answerPosition = TblGameBindingSource.Position      'Stores the quesitons postion in the dataset
+
+        Call IsActive(results(answerPosition))         'Calls the IsActive Functiion 
+
+        incorrectButton.Enabled = False                     'Stops the user from using the incorrect button
+        nextButton.Enabled = True                           'Allows the user to use the next button
+
+    End Sub
+
+    Private Sub incorrectButton_Click(sender As Object, e As EventArgs) Handles incorrectButton.Click
+
+        Dim answered As Integer
+        Dim incorrect As Integer
+        incorrect = 0                       'Assigns zero to the variable incorrect
+
+        For i As Integer = 0 To 8
+
+            If results(i) <> String.Empty Then          'Only counts the queitons answered once
+                answered += 1
+            End If
+
+            If results(i) = "W" Then                    'Counts how many questions are wrong
+                incorrect += 1
+            End If
+        Next
+
+
+        MessageBox.Show("Number Incorrect: " & incorrect.ToString & " incorrect out of " & answered.ToString, 'A message box to show how many the user has gotten incorrect
+                        "Number Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Information)                          'compared to the number of quesitons the user has answered
+
+    End Sub
+
+    Private Sub changeButton_Click(sender As Object, e As EventArgs) Handles changeButton.Click
+        questionNumLabel.Text = "Question " + (TblGameBindingSource.Position + 1).ToString + ":"
+
+        Dim answerPosition As Integer
+        answerPosition = TblGameBindingSource.Position      'Stores the quesitons postion in the dataset
+
+        results(answerPosition) = String.Empty
+
+
+        Call IsActive(results(answerPosition))         'Calls the IsActive Function 
+
+
+
+    End Sub
+End Class
